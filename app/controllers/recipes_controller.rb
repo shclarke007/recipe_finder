67575047645783ff4baa70bfd8f16ff7e2c155e8
Shaneil C <shaneilske@outlook.com>
2025-05-14 21:47:53 +0100

    added recipe search functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,19 +1,29 @@
+
 class RecipesController < ApplicationController
-  def index;end
+  def index; end
 
   def search
-    @ingredients = params[:ingredients] 
+    @ingredients = search_params[:ingredients] 
 
     if @ingredients.present?
-      @recipes = Recipe.search_by_ingredients(@ingredients)
+      @search_results = Recipe.search_by_ingredients(@ingredients)
+
+      @pagy, @recipes = pagy(@search_results, items: 10)
+      render :search_results
     else
       @recipes = [] 
+      render :index
     end
-
-    render :search_results
   end
 
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  private
+
+  def search_params
+    params.permit(:ingredients)
+  end
+  
 end
