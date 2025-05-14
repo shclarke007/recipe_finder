c8514cf0af769d206859d129e351cc5cc0d08336
Shaneil C <shaneilske@outlook.com>
2025-05-14 21:47:12 +0100

    added recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,15 @@
 class Recipe < ApplicationRecord
+  def self.search_by_ingredients(ingredient_list)
+    return none if ingredient_list.blank?
+
+    ingredients = ingredient_list.split(',').map(&:strip).reject(&:blank?)
+    return none if ingredients.empty?
+
+    query = where('') 
+    ingredients.each do |ingredient|
+      query = query.where("ingredients ILIKE ?", "%#{ingredient}%")
+    end
+
+    query
+  end
 end
