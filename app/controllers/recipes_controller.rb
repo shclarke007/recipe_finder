1b4ac6818aa99d204d33374298d212bda7143900
Shaneil C <shaneilske@outlook.com>
2025-05-16 00:47:29 +0100

    updated recipe search

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,15 +3,14 @@ class RecipesController < ApplicationController
   def index; end
 
   def search
-    @ingredients = search_params[:ingredients] 
-    
+    @ingredients = params[:ingredients]
+
     if @ingredients.present?
       set_search_results
-      render :search_results
     else
       @recipes = [] 
-      render :index
     end
+    render :search_results
   end
 
   def show
@@ -22,11 +21,7 @@ class RecipesController < ApplicationController
   end
 
   private
-
-  def search_params
-    params.permit(:ingredients)
-  end
-
+  
   def set_search_results
     @search_results = Recipe.search_by_ingredients(@ingredients)
     @pagy, @recipes = pagy(@search_results, items: 10)
