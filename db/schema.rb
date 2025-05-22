13666939e296b867ab39aa76a6e5b0c8221a94ae
Shaneil C <shaneilske@outlook.com>
2025-05-22 14:13:51 +0100

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_14_120026) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_15_235459) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_14_120026) do
     t.string "category"
     t.string "cuisine"
     t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string "ingredients"
+    t.string "category"
+    t.string "author"
+    t.string "ratings"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
